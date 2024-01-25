c7cf59a2040b6f90e20f3bdc950aa14de374f9ba
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-01-25 20:53:18 +0530

    profile-card-completed

--- profile-component/src/App.jsx
+++ profile-component/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 import { ProfileCard } from './components/ProfileCard'
 
 function App() {
 
   return (
-    <div>
+    <div className="bg-slate-500 flex justify-center min-h-screen items-center">
       <ProfileCard></ProfileCard>
     </div>
   )
--- profile-component/src/components/ProfileCard.jsx
+++ profile-component/src/components/ProfileCard.jsx
@@ -1,8 +1,8 @@
 
 
 export function ProfileCard() {
-    return <div className='w-96 h-[400px] border-2 border-slate-300'>
-        <div className="h-40 bg-teal-400">
+    return <div className='w-96 h-[420px] bg-white rounded-2xl'>
+        <div className="h-44 bg-teal-400 rounded-t-2xl">
         </div>
         <div>
             <div className="flex justify-center -mt-16">
@@ -12,17 +12,17 @@ export function ProfileCard() {
             </div>
             <div className="grid grid-cols-1 mt-8">
                 <div className="flex justify-center">
-                    <span className="text-slate-700 text-xl font-bold">Rita Correia <span className="text-slate-400 font-medium">32</span></span>  <br></br>
+                    <span className="text-slate-700 text-xl font-bold">Rita Correia <span className="text-slate-500 font-medium">32</span></span>  <br></br>
                 </div>
                 <div className="basis-11/12 mt-2  flex justify-center">
-                    <span className="text-xl text-slate-400 font-medium">India</span>
+                    <span className="text-xl text-slate-500 font-medium">India</span>
                 </div>
             </div>
         </div>
         <div className="flex flex-row mt-6 border-t-2 border-slate-300 text-center pt-4">
-            <div className="basis-1/3"><span>80K</span><br></br>Followers</div>
-            <div className="basis-1/3"><span>803K</span><br></br>Likes</div>
-            <div className="basis-1/3"><span>1.4K</span><br></br>Photos</div>
+            <div className="basis-1/3"><span className="text-slate-700 text-xl font-bold">80K</span><br></br><span className="text-slate-500 font-medium text-sm">Followers</span></div>
+            <div className="basis-1/3"><span className="text-slate-700 text-xl font-bold">803K</span><br></br><span className="text-slate-500 font-medium text-sm">Likes</span></div>
+            <div className="basis-1/3"><span className="text-slate-700 text-xl font-bold">1.4K</span><br></br><span className="text-slate-500 font-medium text-sm">Photos</span></div>
         </div>
     </div>
 }
