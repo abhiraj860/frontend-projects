43feecf18fc599e21312e4a0275324abb7cd2f94
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-01-26 14:49:21 +0530

    tailwind-files-added

--- para-generator/src/components/Outputs.jsx
+++ para-generator/src/components/Outputs.jsx
@@ -3,7 +3,7 @@ import { paragraph } from "../atoms/selectors";
 
 export default function Outputs() {
     const para = useRecoilValue(paragraph);
-    return <div>
+    return <div className="bg-slate-500">
         {para}
     </div>
 }
