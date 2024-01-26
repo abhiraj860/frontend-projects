e60283cda6b218ad180075a7a8bb2148817004a3
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-01-26 11:46:37 +0530

    basic-project-done

---
+++ para-generator/src/components/Inputs.jsx
@@ -0,0 +1,6 @@
+export default function Inputs() {
+    return <div>
+        <input type="text" placeholder="Enter number of words"/>
+        <button>Generate</button>
+    </div>
+}
---
+++ para-generator/src/components/Outputs.jsx
@@ -0,0 +1,5 @@
+export default function Outputs() {
+    return <div>
+        dsfsdf
+    </div>
+}
